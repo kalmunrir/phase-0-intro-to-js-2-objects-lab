d286d4f8b033be8d7e977648a2bc607b6dbe3bbb
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-07-21 13:36:11 -0600

    Finished lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Henry Henricson",
+    streetAddress: "801 Henris St",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updEmployee = {...employee};
+    updEmployee[key] = value;
+    return updEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const updEmployee = {...employee};
+    delete updEmployee[key];
+    return updEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
